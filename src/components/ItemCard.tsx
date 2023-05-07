06418ca1a5df8379ae127550a516d510c9ebce16
rachalsmith <66318063+rachalSmith@users.noreply.github.com>
2023-05-07 10:44:21 +0100

    Update image to use  semantically richer component

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -2,6 +2,7 @@ import Card from "@mui/material/Card";
 import { IShopItem } from "../pages/Shop";
 import CardContent from "@mui/material/CardContent";
 import Box from "@mui/material/Box";
+import { CardMedia } from "@mui/material";
 
 interface ItemCardProps {
   item: IShopItem;
@@ -12,16 +13,17 @@ const ItemCard = ({ item }: ItemCardProps) => {
     <>
       <Card variant='outlined' sx={{ border: "red solid" }}>
         <CardContent sx={{ background: "gray" }}>
-          <Box
+          <CardMedia
+            image={item.image}
+            title={item.title}
             sx={{
               objectFit: "scale-down",
               aspectRatio: "2/2.7",
               backgroundPosition: "50% 50%",
               backgroundRepeat: "no-repeat",
               backgroundSize: "cover",
-              backgroundImage: `url(${item.image})`,
             }}
-          ></Box>
+          ></CardMedia>
         </CardContent>
       </Card>
     </>
